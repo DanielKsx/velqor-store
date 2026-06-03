cc923c33238d7ab5b85a35e3b34168efab7b40b1
Daniel Kowalczyk <kostex2@gmail.com>
2026-06-03 10:54:37 +0200

    Implement products API

--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,15 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) { }
 
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
   async findAll() {
     return this.prisma.product.findMany({
@@ -37,11 +32,7 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+
+
 }
